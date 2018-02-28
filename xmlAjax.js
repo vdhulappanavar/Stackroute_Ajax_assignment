af6a0fb5011ab429d677e4962fe8d0b009698842
vdhulappanavar <vdhulappanavar@Vageeshs-MacBook-Air.local>
2018-02-28 12:33:16 +0530

    add to collection and search basic working.. Need to work on layout

--- xmlAjax.js
+++ xmlAjax.js
@@ -1,4 +1,47 @@
+function getlistele(data){
+  return `
+  <a href="#" class="list-group-item">
+      <h4 class="list-group-item-heading">${data.title}</h4>
+      <p class="list-group-item-text">WhatEver</p>
+  </a>
+  `
+}
+
+
+function loadcollection(_collections){
+  let collectionList = document.getElementById("collectionList");
+  for(i in _collections){
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET" ,`https://api.themoviedb.org/3/movie/${_collections[i]}?api_key=4a9f640e502709dc6bcd23286de1426e&language=en-US` , true)
+    xhttp.send()
+    xhttp.onreadystatechange= function(){
+      if(xhttp.readyState==4 && xhttp.status == 200){
+        collectionList.innerHTML += getlistele(JSON.parse(xhttp.responseText))
+      }
+    }
+  }
+}
+function foo2(id){
+  console.log(id)
+  //collection.push(id)
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("GET", `https://api.themoviedb.org/3/movie/${id}?api_key=4a9f640e502709dc6bcd23286de1426e&language=en-US` , true)
+  xhttp.send()
+  xhttp.onreadystatechange = function(){
+    console.log("readstaechange")
+    if(xhttp.readyState==4 && xhttp.status==200){
+      //foo1(JSON.parse(xhttp.responseText))
+      let collectionList = document.getElementById("collectionList");
+      collectionList.innerHTML += getlistele(JSON.parse(xhttp.responseText))
+    }
+  }
+  
+}
+
 function init(){
+  collection = [394117 , 353616]
+  
+  loadcollection(collection)
   apiKey = '4a9f640e502709dc6bcd23286de1426e';
   console.log("in init");
   let xhttp = new XMLHttpRequest();
@@ -14,21 +57,92 @@ function init(){
   xhttp.send(); 
 }
 
-function createRow(data){
+function foo1(mdata){
+  let modalData = document.getElementById("movieData1");
+  
+  data=`
+        <h1 class="text-primary">${mdata['title']}</h1>
+        <h3 class="text-success">Rating : ${mdata['vote_average']}</h3>
+        <div class="text-center">
+        </div>
+  `
+  modalData.innerHTML = data
+}
+
+function foo(data){
+  console.log(data)
+  document.getElementById("movieData").innerHTML = data;
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("GET", `https://api.themoviedb.org/3/movie/${data}?api_key=4a9f640e502709dc6bcd23286de1426e&language=en-US` , true)
+  xhttp.send()
+  xhttp.onreadystatechange = function(){
+    console.log("readstaechange")
+    if(xhttp.readyState==4 && xhttp.status==200){
+      foo1(JSON.parse(xhttp.responseText))
+    }
+  }
+}
+
+
+
+function createRow(data , pos){
+  console.log("createRow")
+  //console.log(data[pos].poster_path)
   let row = "";
   row += `<section class="row">`
-  for(let i=0; i<3; i++){
-    row+=`<article class="col-md-4"
-              <div class="col-md-2">
-                <img src=https://image.tmdb.org/t/p/w500/${data[i].poster_path}/>
-              </div>
-              <div class="col-md-2">
-                results[i].titl
-              </div>    
-          </article>
-    `
+  for(let i=pos; i<pos+3; i++){
+    console.log(i)
+    //console.log(data[i].poster_path)
+    //console.log(data[i].poster_path)
+    console.log(data[i])
+    //result = JSON.parse(data[i])
+    if(data[i]){ console.log("inside if")
+    console.log(data[i].poster_path)
+      row+=`<article class="col-md-3">
+               <div class="panel panel-default panel-front">
+                  <div class=" panel-heading">
+                    <img class="img-responsive col-md-3" src="https://image.tmdb.org/t/p/w500/${data[i].poster_path}"/>
+                  </div>
+                  <div class="panel-body">
+                    <a onclick="foo(this.id)" id="${data[i].id}" data-toggle="modal" data-target="#myModal"><h3 class="text-primary">${data[i].title}</h3></a>
+                    <h4>Rating : ${data[i].vote_average}</h4>
+                  </div>  
+                  <div class="panel-footer">
+                      <button id="${data[i].id}"class="btn btn-danger" onclick="foo2(this.id)">+ Add</button>
+                  </div>
+                <div>  
+            </article>
+      `
+      }
   }
   row+="<section>"
+  return row;
+}
+function createRow1(data , pos){
+  console.log("createRow")
+  //console.log(data[pos].poster_path)
+  let row = "";
+  row += `<section class="row">`
+  
+    console.log(data[pos].poster_path)
+      row+=`<article">
+               <div class="panel panel-default panel-front">
+                  <div class=" panel-heading">
+                    <img class="img-responsive img-thumbnail" width="400" height="400"  src="https://image.tmdb.org/t/p/w500/${data[pos].poster_path}"/>
+                  </div>
+                  <div class="panel-body">
+                    <a onclick="foo(this.id)" id="${data[pos].id}" data-toggle="modal" data-target="#myModal"><h3 class="text-primary">${data[pos].title}</h3></a>
+                    <h4>Rating : ${data[pos].vote_average}</h4>
+                  </div>  
+                  <div class="panel-footer">
+                      <button id="${data[pos].id}"class="btn btn-danger" onclick="foo2(this.id)">+ Add</button>
+                  </div>
+                <div>  
+            </article>
+      `
+     
+  row+="<section>"
+  return row;
 }
 function showUpcomingMovieData(data){
   //console.log("inshowUpcomingMovieData" )
@@ -38,7 +152,7 @@ function showUpcomingMovieData(data){
   if(data){    
     results = (data['results'])
     if(results){
-      for(let i in results){
+      /*for(let i in results){
         //result = JSON.parse(results[i])
         console.log(typeof(results[i]))
         console.log(results[i])
@@ -50,6 +164,10 @@ function showUpcomingMovieData(data){
         if(i%3 == 0){
           divForUpcomingMovie.innerHTML+='</section>'
         }
+      }*/
+
+      for(let i=0; i<results.length; i+=3){
+        divForUpcomingMovie.innerHTML += createRow(results , i)
       }
       
     }
@@ -66,7 +184,10 @@ function showUpcomingMovieData(data){
 function showResults(){
   console.log("keyed UP")
   let searchBarInput = document.getElementById("searchInput").value;
-  
+  let splitData = searchBarInput.split(" ");
+  if(splitData.length>1){
+    searchBarInput = splitData.join("%20")
+  }
   
   let selectForInput = document.getElementById("selectValueForInput").value;
   console.log(searchBarInput.value)
@@ -99,12 +220,19 @@ function printSerchResults(data){
   if(results.length < 10)
     len = length(results)
   console.log(data.length)
-  for(let i=0;i<len;i++){
+  /*for(let i=0;i<len;i++){
     console.log(i)
       if(i%3 == 0)
         resultDiv.innerHTML += '<div class="row">'
-      resultDiv.innerHTML += '<div class="col-md-4">'+'<img class="col-md-2 hidden-sm" src="https://image.tmdb.org/t/p/w500/'+results[i].poster_path+'"/><p class="col-md-2 col-sm-4">'+results[i].title+"</p></article>";
+      resultDiv.innerHTML += '<div class="col-md-4">'+'<img class="col-md-2" src="https://image.tmdb.org/t/p/w500/'+results[i].poster_path+'"/><p class="col-md-2 col-sm-4">'+results[i].title+"</p></article>";
       if(i%3 ==0)
         resultDiv.innerHTML += "</div>"
+  }*/
+
+  for(let i=0; i<len; i+=1){console.log(i)
+    resultDiv.innerHTML += createRow1(results , i )
   }
 }
+
+//https://image.tmdb.org/t/p/w500/rDeGK6FIUfVcXmuBdEORPAGPMNg.jpg/
+//https://image.tmdb.org/t/p/w500/3XNfYTW4XGscI81nXMSWGsQ8cpu.jpg
